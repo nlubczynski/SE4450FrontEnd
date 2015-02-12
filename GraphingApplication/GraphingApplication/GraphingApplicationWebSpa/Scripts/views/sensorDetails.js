0a276a44f988e5e0dd68e609900e288b7d35a872
NikLubz <nlubczynski@gmail.com>
2015-02-12 14:51:55 -0500

    Modifying small js

--- GraphingApplication/GraphingApplication/GraphingApplicationWebSpa/Scripts/views/sensorDetails.js
+++ GraphingApplication/GraphingApplication/GraphingApplicationWebSpa/Scripts/views/sensorDetails.js
@@ -1,12 +1,17 @@
 $(function () {
     appFsMvc.SensorsDetailsView = Backbone.View.extend({
 
-        render: function () {
-            appFsMvc.utility.renderTemplate("sensorDetail.htm",
-                $(this.el),
-                { data: this.model.toJSON() }
-            );
+        events: {
+            "click #createBuilding": "gotoCreateView"
         },
+
+        render: function () {
+            appFsMvc.utility.renderTemplate("sensorDetail.htm", $(this.el), { data: this.model.toJSON() });
+        },
+
+        gotoCreateView: function () {
+            appFsMvc.App.navigate("create", { trigger: true });
+        }
 
     });
 });
