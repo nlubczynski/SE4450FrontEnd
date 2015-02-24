5f7de85af9ba0cb2268ab59fcbc50906afc76ee4
Brandon McRae <bmcrae2@uwo.ca>
2015-02-24 10:11:48 -0500

    - adds routing and javascript to track a checkbox to select lambda vs database

--- GraphingApplication/GraphingApplication/GraphingApplicationWebAppSpa/Controllers/SensorReadingsController.fs
+++ GraphingApplication/GraphingApplication/GraphingApplicationWebAppSpa/Controllers/SensorReadingsController.fs
@@ -3,17 +3,36 @@
 open System.Web.Http
 open FsWeb.Models
 open FsWeb.Repositories
- 
+open System
+
 type SensorReadingsController(repository : SensorReadingsRepository) =
     inherit ApiController()
  
     new() = new SensorReadingsController(new SensorReadingsRepository())
  
-    member x.Get() = 
-        repository.GetAll()
+    member x.Get(lambda: bool) = 
+        match lambda with
+            | true -> 
+                printfn ("Lambda is active1")
+                repository.GetAll()
+            | false ->
+                printfn ("MySQL database is active1")
+                repository.GetAll()
 
-    member x.Get(id : string, action : string, timestamp : string) = 
-        // Routing table        
-        if      action = "GetOne"      then repository.GetOne(id)
-        elif    action = "GetAfter"    then repository.GetOneAfterTime(id, timestamp)
-        else                                [||]
+    member x.Get(id : string, action : string, timestamp : string, lambda: bool) = 
+        // Routing table
+        printfn("Selecting lambda or mysql: ")
+
+        match lambda with 
+            | true -> 
+                printfn ("Lambda is active2")
+                if      action = "GetOne"      then repository.GetOne(id)
+                elif    action = "GetAfter"    then repository.GetOneAfterTime(id, timestamp)
+                else    [||]
+        
+            | false -> 
+                printfn ("MySQL database is active2")
+                if      action = "GetOne"      then repository.GetOne(id)
+                elif    action = "GetAfter"    then repository.GetOneAfterTime(id, timestamp)
+                else    [||]
+                
