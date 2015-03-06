97f5cd29c57f96ed15b92bfc99591fda23399530
Nik Lubczynski <nlubczynski@gmail.com>
2015-03-06 15:48:15 -0500

    Updated to new architecture for front end app

---
+++ GraphingApplication/FrontEndApp/Global.asax.cs
@@ -0,0 +1,158 @@
+﻿using FrontEndApp.Models;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Timers;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace FrontEndApp
+{
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        // Main repository
+        private Powersmiths data;
+
+        // Data sets
+        private List<Sensor> _sensors;
+        public List<Sensor> Sensors
+        {
+            get
+            {
+                sensorMutex.WaitOne();
+                List<Sensor> returnVal = _sensors.ConvertAll(sensor => new Sensor(sensor));
+                sensorMutex.ReleaseMutex();
+                return returnVal;
+            }
+            private set { _sensors = value; }
+        }
+
+        private List<SensorReading> _sensorReadings;
+        public List<SensorReading> SensorReadings
+        {
+            get
+            {
+                sensorReadingMutex.WaitOne();
+                List<SensorReading> returnVal = _sensorReadings.ConvertAll(sensorReading => new SensorReading(sensorReading));
+                sensorReadingMutex.ReleaseMutex();
+                return returnVal;
+            }
+            private set { _sensorReadings = value; }
+        }
+
+        private List<Building> _buidlings;
+        public List<Building> Buildings
+        {
+            get
+            {
+                buildingMutex.WaitOne();
+                List<Building> returnVal = _buidlings.ConvertAll(building => new Building(building));
+                buildingMutex.ReleaseMutex();
+                return returnVal;
+            }
+            private set { _buidlings = value; }
+        }
+
+        private List<Unit> _units;
+        public List<Unit> Units
+        {
+            get
+            {
+                unitMutex.WaitOne();
+                List<Unit> returnVal = _units.Select(unit => unit).ToList();
+                unitMutex.ReleaseMutex();
+                return returnVal;
+            }
+            private set { _units = value; }
+        }
+
+        // Instance
+        public static MvcApplication Instance { get; private set; }
+
+        // Mutext for retrieving data
+        private static Mutex buildingMutex = new Mutex();
+        private static Mutex sensorMutex = new Mutex();
+        private static Mutex sensorReadingMutex = new Mutex();
+        private static Mutex unitMutex = new Mutex();
+
+        // Main application entry point
+        protected void Application_Start()
+        {
+            if (Instance == null)
+                Instance = this;
+
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Initiliaze
+            data = new Powersmiths();
+            Sensors = new List<Sensor>();
+            SensorReadings = new List<SensorReading>();
+            update();
+
+            // Update loop
+            Thread updateThread = new Thread(updateLoop);
+            updateThread.Start();
+        }
+
+        private void updateLoop()
+        {
+            for (; ; )
+            {
+                update();
+            }
+        }
+
+        private void update()
+        {
+            List<Sensor> swapSensors                = new List<Sensor>();
+            List<SensorReading> swapSensorReadings  = new List<SensorReading>();
+            List<Building> swapBuildings            = new List<Building>();
+            List<Unit> swapUnits                    = new List<Unit>();
+
+
+            // Sensors
+            swapSensors = data.Sensors.ToList();
+
+            // Sensor Readings
+            swapSensorReadings = data.SensorReadings.ToList();
+
+            // Buildings
+            swapBuildings = data.Buildings.ToList();
+
+            // Units
+            swapUnits = data.Units.ToList();
+
+            // Swap Buildings
+            buildingMutex.WaitOne();
+            Buildings = swapBuildings;
+            buildingMutex.ReleaseMutex();
+
+            // Swap sensors
+            sensorMutex.WaitOne();
+            Sensors = swapSensors;
+            sensorMutex.ReleaseMutex();
+
+            // Swap sensorReadings
+            sensorReadingMutex.WaitOne();
+            SensorReadings = swapSensorReadings;
+            sensorReadingMutex.ReleaseMutex();
+
+            // Swap units
+            unitMutex.WaitOne();
+            Units = swapUnits;
+            unitMutex.ReleaseMutex();
+        }
+
+    }
+}
---
+++ GraphingApplication/FrontEndApp/Scripts/Views/home.js
@@ -0,0 +1,160 @@
+﻿$(function () {
+    /**
+     * Load new data depending on the selected min and max
+     */
+    var mySqlLoading = false;
+    var lambdaLoading = false;
+    function afterSetExtremes(e) {
+
+        var MySQL = $('#MySQL').highcharts();
+        var Lambda = $('#Lambda').highcharts();
+
+        MySQL.showLoading('Loading data from server...');
+        Lambda.showLoading('Loading data from server...');
+
+        if (mySqlLoading != true) {
+            mySqlLoading = true;
+            $.getJSON('http://www.highcharts.com/samples/data/from-sql.php?start=' + Math.round(e.min) +
+                    '&end=' + Math.round(e.max) + '&callback=?', function (data) {
+
+                        MySQL.series[0].setData(data);
+                        MySQL.hideLoading();
+                        mySqlLoading = false;
+                    });
+        };
+
+        if (lambdaLoading != true) {
+            lambdaLoading = true;
+            $.getJSON('http://www.highcharts.com/samples/data/from-sql.php?start=' + Math.round(e.min) +
+                    '&end=' + Math.round(e.max) + '&callback=?', function (data) {
+
+                        Lambda.series[0].setData(data);
+                        Lambda.hideLoading();
+                        lambdaLoading = false;
+                    });
+        }
+    }
+
+    $.getJSON('http://www.highcharts.com/samples/data/from-sql.php?callback=?', function (data) {
+
+        // Add a null value for the end date
+        data = [].concat(data, [[Date.UTC(2011, 9, 14, 19, 59), null, null, null, null]]);
+
+        // create the chart
+        $('#MySQL').highcharts('StockChart', {
+            chart: {
+                type: 'candlestick',
+                zoomType: 'x'
+            },
+
+            navigator: {
+                adaptToUpdatedData: false,
+                series: {
+                    data: data
+                }
+            },
+
+            scrollbar: {
+                liveRedraw: false
+            },
+
+            title: {
+                text: 'MySQL Database'
+            },
+
+            rangeSelector: {
+                buttons: [{
+                    type: 'hour',
+                    count: 1,
+                    text: '1h'
+                }, {
+                    type: 'day',
+                    count: 1,
+                    text: '1d'
+                }, {
+                    type: 'month',
+                    count: 1,
+                    text: '1m'
+                }, {
+                    type: 'year',
+                    count: 1,
+                    text: '1y'
+                }, {
+                    type: 'all',
+                    text: 'All'
+                }],
+                inputEnabled: false, // it supports only days
+                selected: 4 // all
+            },
+
+            xAxis: {
+                events: {
+                    afterSetExtremes: afterSetExtremes
+                },
+                minRange: 3600 * 1000 // one hour
+            },
+
+            yAxis: {
+                floor: 0
+            },
+
+            series: [{
+                data: data,
+                dataGrouping: {
+                    enabled: false
+                }
+            }]
+        });
+    });
+
+    $.getJSON('http://www.highcharts.com/samples/data/from-sql.php?callback=?', function (data) {
+
+        // Add a null value for the end date
+        data = [].concat(data, [[new Date(), null, null, null, null]]);
+
+        // create the chart
+        $('#Lambda').highcharts('StockChart', {
+            chart: {
+                type: 'candlestick',
+                zoomType: 'x'
+            },
+
+            navigator: {
+                adaptToUpdatedData: false,
+                series: {
+                    data: data
+                }
+            },
+
+            scrollbar: {
+                liveRedraw: false
+            },
+
+            title: {
+                text: 'Lambda Architecture'
+            },
+
+            rangeSelector: {
+                enabled: false
+            },
+
+            xAxis: {
+                events: {
+                    afterSetExtremes: afterSetExtremes
+                },
+                minRange: 3600 * 1000 // one hour
+            },
+
+            yAxis: {
+                floor: 0
+            },
+
+            series: [{
+                data: data,
+                dataGrouping: {
+                    enabled: false
+                }
+            }]
+        });
+    });
+});
